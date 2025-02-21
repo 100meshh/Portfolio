f5db7aacdccf1c60bb0b9e2c87e42c20bb1ec7a6
Somesh Shukla <someshshuklacollege@gmail.com>
2025-02-21 12:01:56 +0530

    almost completed

--- script.js
+++ script.js
@@ -24,6 +24,13 @@ window.onscroll = () => {
                 links.classList.remove(`active`);
                 document.querySelector(`header nav a[href*="${id}"]`).classList.add(`active`);
             })
+            // active sessions for animation on scroll 
+            sec.classList.add('show-animate');
+        }
+        // if want to use animation that repeat on scroll use this 
+        else {
+            sec.classList.remove('show-animate');
+
         }
     });
     let header = document.querySelector(`.header`);
